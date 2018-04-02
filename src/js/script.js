49cf0e9e828a85bbca9bf274dee162cc8857b692
Sylwuchaa <uzumagi1@gmail.com>
2018-04-02 13:39:46 +0200

    portfolio in work

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+
+// $("#scroll").on("click", function(){
+//     $("body").animate({
+//         scrollTop: 400
+//     }, 1000)
+
+// });
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
